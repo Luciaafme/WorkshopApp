739b39386a6a17f33ebaa11af64809abe76086b2
lucia <luciafme@gmail.com>
2023-10-22 11:43:17 +0100

    5-Classes Mechanic and RepairManager developed

--- workshop/src/org/ulpgc/is1/model/Mechanic.java
+++ workshop/src/org/ulpgc/is1/model/Mechanic.java
@@ -1,4 +1,43 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mechanic {
+
+    public String name;
+    public String surname;
+
+    public List<Repair> repairList;
+
+
+    public Mechanic(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+        repairList = new ArrayList<Repair>();
+    }
+
+    public void addRepair(Repair repair){
+        if(!repairList.contains(repair)) repairList.add(repair);
+    }
+
+    public Repair getRepair(int index){
+        return repairList.get(index);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
--- workshop/src/org/ulpgc/is1/model/RepairManager.java
+++ workshop/src/org/ulpgc/is1/model/RepairManager.java
@@ -1,4 +1,80 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Date;
+
 public class RepairManager {
+
+    private List<Mechanic> mechanicList;
+    private List<Vehicle> vehicleList;
+
+    private List<SparePart> sparePartList;
+
+    public RepairManager() {
+        mechanicList = new ArrayList<Mechanic>();
+        vehicleList = new ArrayList<Vehicle>();
+        sparePartList = new ArrayList<SparePart>();
+    }
+
+    public void addSparePart(String name, int price){
+        SparePart sparePart = new SparePart(name, price);
+        if(!sparePartList.contains(sparePart)) sparePartList.add(sparePart);
+    }
+    public List<SparePart> getSparePart(){
+        return sparePartList;
+    }
+
+    public void addMechanic(String name, String surname){
+        Mechanic mechanic = new Mechanic(name, surname);
+        if(!mechanicList.contains(mechanic)) mechanicList.add(mechanic);
+    }
+    public void addVehicle(String make, String model, Plate plate){
+        Vehicle vehicle = new Vehicle(make, model, plate);
+        if(!vehicleList.contains(vehicle))vehicleList.add(vehicle);
+    }
+
+
+    public Mechanic getMechanic(int index){
+
+        return mechanicList.get(index);
+    }
+
+    public Vehicle getVehicle(int index){
+
+        return vehicleList.get(index);
+    }
+
+    public int countVehicle(){
+
+        return vehicleList.size();
+    }
+
+
+    public void repair(Vehicle vehicle, Mechanic mechanic, Date date, String description, int effort, BreakdownTypes breakdownType, List<SparePart> sparePart,  List<Item> itemList) {
+
+        Repair repair = new Repair(date, description, effort, breakdownType);
+        mechanic.addRepair(repair);
+        repair.addMechanic(mechanic);
+
+        vehicle.addRepair(repair);
+        repair.addVehicle(vehicle);
+
+
+        for(int i=0; i<sparePart.size();i++){
+            sparePart.get(i).addRepair(repair);
+            repair.addSparePart(sparePart.get(i));
+            itemList.get(i).setSparePart(sparePart.get(i));
+            sparePart.get(i).addItem(itemList.get(i));
+            repair.addItem(itemList.get(i));
+        }
+
+    }
+
+
+    public void removeVehicle(int index){
+
+        vehicleList.remove(index);
+    }
+
 }
