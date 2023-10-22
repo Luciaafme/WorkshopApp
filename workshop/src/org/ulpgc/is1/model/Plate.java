ad7e28a4527ea7c564e4832838884d913dd3527e
lucia <luciafme@gmail.com>
2023-10-22 11:33:48 +0100

    3-Class Vehicle and Plate

--- workshop/src/org/ulpgc/is1/model/Plate.java
+++ workshop/src/org/ulpgc/is1/model/Plate.java
@@ -1,4 +1,29 @@
 package org.ulpgc.is1.model;
 
 public class Plate {
+    public String number;
+
+    public Plate(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public boolean isValid(){
+
+        if(number.matches("^\\d{4}[A-Z]{3}$")){
+            return true;
+        }
+        else{
+            this.number = "XXXX";
+            return false;
+        }
+    }
 }
+
--- workshop/src/org/ulpgc/is1/model/Vehicle.java
+++ workshop/src/org/ulpgc/is1/model/Vehicle.java
@@ -18,4 +18,37 @@ public class Vehicle {
         this.plate = plate;
         repairList = new ArrayList<Repair>();
     }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Plate getPlate() {
+        return plate;
+    }
+
+    public void setPlate(Plate plate) {
+        this.plate = plate;
+    }
+
+    public void addRepair(Repair repair){
+
+        if(!repairList.contains(repair))repairList.add(repair);
+    }
+
+    public void addCustomer(Customer customer){
+        this.customer = customer;
+    }
 }
