812fe4bd6c1b0081014c76639564ac33ca150c50
lucia <luciafme@gmail.com>
2023-10-22 11:35:57 +0100

    4-Class Customer and Phone developed

--- workshop/src/org/ulpgc/is1/model/Customer.java
+++ workshop/src/org/ulpgc/is1/model/Customer.java
@@ -1,4 +1,36 @@
 package org.ulpgc.is1.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+    public String name;
+    public Phone phone;
+
+    public List<Vehicle> owner;
+    public Customer(String name, Phone phone) {
+        this.name = name;
+        this.phone = phone;
+        owner = new ArrayList<Vehicle>();
+    }
+    public void addVehicle(Vehicle vehicle){
+
+        if (!owner.contains(vehicle)) owner.add(vehicle);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
 }
--- workshop/src/org/ulpgc/is1/model/Phone.java
+++ workshop/src/org/ulpgc/is1/model/Phone.java
@@ -1,4 +1,28 @@
 package org.ulpgc.is1.model;
 
 public class Phone {
+    public String number;
+
+    public Phone(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public boolean isValid(){
+        String PhoneNumber = number.replaceAll("[\\s-]+", "");
+        if(PhoneNumber.matches("\\d{9}")){
+            return true;
+        }
+        else{
+            this.number = "XXXX";
+            return false;
+        }
+    }
 }
